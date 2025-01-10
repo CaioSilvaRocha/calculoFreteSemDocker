35b15aac2c4b0e6a711fd485e3896fc779d11c26
Caio Rocha <109962498+CaioSilvaRocha@users.noreply.github.com>
2025-01-10 10:44:30 -0300

    Mudanças (consultasBancoDados, console.log)

--- servidor.js
+++ servidor.js
@@ -1,11 +1,11 @@
 const app = require("./componentesServidor/app");
 const dotenv = require("dotenv");
-const consultasBancoDados = require("./bancoDados/consultasBancoDados");
+//const consultasBancoDados = require("./bancoDados/consultasBancoDados");
 
 dotenv.config();
 
-consultasBancoDados.criarBancoDados();
+//consultasBancoDados.criarBancoDados();
 
 app.listen(process.env.portaServidor, () => {
-    console.log(`\nURL do Servidor: http://${process.env.host}:${process.env.portaServidor}/`);
+    console.log(`\nServidor ativo!`);
 });
