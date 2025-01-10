95255a03376cfb7a7706d4e9b6cccd990f02963e
Caio Rocha <109962498+CaioSilvaRocha@users.noreply.github.com>
2025-01-10 13:09:33 -0300

    Mudanças (express.static)

--- componentesServidor/app.js
+++ componentesServidor/app.js
@@ -5,9 +5,13 @@ const path = require("path");
 
 const app = express();
 
-app.use("/CSS", express.static(path.dirname(__dirname) + "\\public\\CSS"));
+/*app.use("/CSS", express.static(path.dirname(__dirname) + "\\public\\CSS"));
 app.use("/HTML", express.static(path.dirname(__dirname) + "\\public\\HTML"));
 app.use("/JavaScript", express.static(path.dirname(__dirname) + "\\public\\JavaScript"));
+*/
+app.use("/CSS", express.static("public/CSS"));
+app.use("/HTML", express.static("public/HTML"));
+app.use("/JavaScript", express.static("public/JavaScript"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(rotas);
